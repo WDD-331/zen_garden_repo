5f5854902ac66aaf6f2a157ee77933207a165218
chelsiehill <chelsie9@byui.edu>
2019-10-18 18:58:32 -0600

    Testing

--- styleGuide/style-guide-template-parcel/style-guide-template/build/modernizr.4ede9213.js
+++ styleGuide/style-guide-template-parcel/style-guide-template/build/modernizr.4ede9213.js
@@ -715,7 +715,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55794" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62464" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
